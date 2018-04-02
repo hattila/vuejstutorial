e145f1ef01e46ad158cfbe40d93398146d36825c
Attila Hejjas <attila.h9@gmail.com>
2018-04-02 18:22:24 +0200

    v for, v if

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="hello">
-    {{ name }}
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+      </ul>
 
-    {{ btnState ? 'The button is disabled' : 'The button is active' }}
-
-    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>
+      <p v-if="skills.length >= 1">You have more than one skills</p>
+      <p v-else>You have less than or equal to 1 skill.</p>
+    </div>
   </div>
 </template>
 
@@ -12,10 +15,12 @@
 export default {
   name: 'Skills',
   data() {
-      return {
-          name: 'Hattila',
-          btnState: true
-      }
+    return {
+      skills: [
+        {"skill": "vue.js"},
+        {"skill": "frontend developer"},
+      ]
+    }
   }
 }
 </script>
